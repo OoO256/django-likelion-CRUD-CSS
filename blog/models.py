a97e28d8a6362152f84121f213b4f97b29c669ef
Yonguk Lee <qjrmsktso2@gmail.com>
2019-05-02 11:47:10 +0000

    done

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+from django.utils import timezone
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField(default=timezone.now)
+    body = models.TextField(default='')
+    author = models.TextField(default='')
+    img = models.TextField(default='')
