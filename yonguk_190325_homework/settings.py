05e4dd4e013944f37c1a44caaec78f8d6c4ec5a0
Yonguk Lee <qjrmsktso2@gmail.com>
2019-07-31 17:35:14 +0900

    Update settings.py

--- yonguk_190325_homework/settings.py
+++ yonguk_190325_homework/settings.py
@@ -127,5 +127,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
