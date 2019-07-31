d03b97bc8296f31ede400b5e8267febc673ff58e
OoO256 <qjrmsktso2@gmail.com>
2019-07-31 18:50:34 +0900

    change static root

--- yonguk_190325_homework/settings.py
+++ yonguk_190325_homework/settings.py
@@ -128,9 +128,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
