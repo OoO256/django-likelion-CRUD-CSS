1da385121c7de34847d80ec22d47e8b4c87f7463
Yonguk Lee <qjrmsktso2@gmail.com>
2019-07-31 17:45:22 +0900

    Update settings.py

--- yonguk_190325_homework/settings.py
+++ yonguk_190325_homework/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fvy6fzv2%$$ynkv*a_zl21udpc5s*&&h+p!0_ztw!)f3(p-%-+'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
